b6bad62203586e5d181a50e2d5863bd8ceae24bd
Vladislav Bobylev <vladislav.a.bobylev@gmail.com>
2016-05-22 16:10:35 +0300

    FIX: refactor code and add "values" is mesh points

--- mesh.cpp
+++ mesh.cpp
@@ -7,6 +7,7 @@ mesh::mesh ()
   width_nodes_count_ = hight_nodes_count_ = nodes_count_ = 0;
   boundary_colomn_ = boundary_row_ = 0;
   nodes_ = 0;
+  values_ = 0;
 }
 
 mesh::mesh (double width, double hight, double cut_hight, double cut_width, int width_nodes_count, int hight_nodes_count, QPointF init_point)
@@ -19,6 +20,7 @@ mesh::mesh (double width, double hight, double cut_hight, double cut_width, int 
   hight_nodes_count_ = hight_nodes_count;
   nodes_count_ = configurate_nodes ();
   nodes_ = new QPointF[nodes_count_];
+  values_ = new double[nodes_count_];
   initial_point_ = init_point;
 
 }
@@ -29,6 +31,10 @@ mesh::~mesh ()
     {
       delete [] nodes_;
     }
+  if (values_)
+  {
+      delete [] values_;
+  }
 }
 
 int mesh::configurate_nodes ()
@@ -36,8 +42,8 @@ int mesh::configurate_nodes ()
   if (width_nodes_count_ <= 0 || hight_nodes_count_ <= 0)
     return 0;                                             //< we haven't nodes
 
-  double dist_width = width_ / width_nodes_count_;
-  double dist_hight = hight_ / hight_nodes_count_;
+  const double dist_width = width_ / width_nodes_count_;
+  const double dist_hight = hight_ / hight_nodes_count_;
 
   boundary_row_ = (hight_ - cut_hight_) / dist_hight;
   boundary_colomn_ = (width_ - cut_width_) / dist_width;
@@ -111,19 +117,57 @@ void mesh::set_nodes_values ()
   // set rectangular upper part
   for (int i = 0; i <= boundary_row_; i++)
     {
-      for (int j = 0; j < boundary_colomn_; j++)
+      for (int j = 0; j < width_nodes_count_; j++)
         {
           nodes_[i * boundary_colomn_ + j].setX (initial_point_.x () + j * step_value_x);
           nodes_[i * boundary_colomn_ + j].setY (initial_point_.y () - i * step_value_y);
         }
     }
-  for (int i = boundary_row_ + 1; i < hight_nodes_count_; i++)
+  for (int i = boundary_row_; i < hight_nodes_count_; i++)
     {
       for (int j = 0; j < width_nodes_count_; j++)
         {
           nodes_[i * width_nodes_count_ + j].setX (initial_point_.x () + j * step_value_x);
           nodes_[i * width_nodes_count_ + j].setY (initial_point_.y () - i * step_value_y);
         }
+  }
+}
+
+int mesh::get_mesh_number (const int pos_x, const int pos_y)
+{
+    int global_mesh_position = 0;
+    if (pos_y > boundary_row_)
+      {
+        global_mesh_position = boundary_colomn_ * width_nodes_count_;
+        global_mesh_position += (pos_y - boundary_row_) * boundary_colomn_;
+      }
+    else
+      {
+        global_mesh_position = pos_y * width_nodes_count_;
+      }
+
+    global_mesh_position += pos_x;
+
+    return global_mesh_position;
+}
+
+int mesh::get_coord_mesh (const int global_pos, int &pos_x, int &pos_y)
+{
+    if (global_pos < 0)
+        return -1;
+    int row = global_pos / width_nodes_count_;
+
+    if (row > boundary_row_)
+    {
+        row = boundary_colomn_ + (global_pos - boundary_row_ * width_nodes_count_) / boundary_colomn_;
+        pos_y = row;
+        pos_x = global_pos - (boundary_row_ * width_nodes_count_ + (pos_y - boundary_row_) * boundary_colomn_);
     }
+    else
+    {
+        pos_y = row;
+        pos_x = global_pos - pos_y * width_nodes_count_;
+    }
+    return 0;
 }
 
--- mesh.h
+++ mesh.h
@@ -8,16 +8,17 @@ class mesh
 
 private:
   QPointF initial_point_;
-  //  _____________
+  // i.p._________
   //  |           |
   //  |           |
   //  |___________|
-  // i.p.
+  //
   double width_, hight_; //< the rectangular size
   double cut_width_, cut_hight_; //< the cut rectangular coordinates
   int width_nodes_count_, hight_nodes_count_; //< the sizes of mesh
-  int boundary_row_, boundary_colomn_;
+  int boundary_row_, boundary_colomn_; //< the last row and colomn in mesh
   QPointF *nodes_; // massive of nodes of mesh
+  double *values_; // massive of valus in nodes
   int nodes_count_;
 
 public:
@@ -34,6 +35,8 @@ public:
   int get_boundary_colomn () const;
   int configurate_mesh (int width_point, int hight_point);
   void set_nodes_values ();
+  int get_mesh_number (const int pos_x, const int pos_y);
+  int get_coord_mesh (const int global_pos, int &pos_x, int &pos_y);
 };
 
 #endif // MESH_H
