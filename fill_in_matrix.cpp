e6bf4b8e828d49957cc99a5f50561481f90355e3
Vladislav Bobylev <vladislav.a.bobylev@gmail.com>
2016-05-25 00:28:28 +0300

    NEW: fill rhs part not completed

--- fill_in_matrix.cpp
+++ fill_in_matrix.cpp
@@ -1,141 +1,210 @@
 #include "fill_in_matrix.h"
 
-void fill_in_matrix::pattern_1 (int *index, const int i, const int j, const int m, const int n)
+#include "multithread.h"
+
+void fill_in_matrix::pattern_1 (int *index, const int i, const int j)
 {
-    index[0] = get_l (i - 1, j, m, n);
-    index[1] = get_l (i - 1, j + 1, m, n);
-    index[2] = get_l (i, j - 1, m, n);
-    index[3] = get_l (i, j + 1, m, n);
-    index[4] = get_l (i + 1, j - 1, m, n);
-    index[5] = get_l (i + 1, j, m, n);
+    index[0] = get_mesh_number (i - 1, j);
+    index[1] = get_mesh_number (i - 1, j + 1);
+    index[2] = get_mesh_number (i, j - 1);
+    index[3] = get_mesh_number (i, j + 1);
+    index[4] = get_mesh_number (i + 1, j - 1);
+    index[5] = get_mesh_number (i + 1, j);
 }
 
-void fill_in_matrix::pattern_2 (int *index, const int i, const int j, const int m, const int n)
+void fill_in_matrix::pattern_2 (int *index, const int i, const int j)
 {
     if (i == 0)
     {
-        index[0] = get_l (i, j + 1, m, n);
-        index[1] = get_l (i + 1, j, m, n);
+        index[0] = get_mesh_number (i, j + 1);
+        index[1] = get_mesh_number (i + 1, j);
     }
     else
     {
-        index[0] = get_l (i - 1, j, m, n);
-        index[1] = get_l (i, j - 1, m, n);
+        index[0] = get_mesh_number (i - 1, j);
+        index[1] = get_mesh_number (i, j - 1);
     }
 }
 
-void fill_in_matrix::pattern_3 (int *index, const int i, const int j, const int m, const int n)
+void fill_in_matrix::pattern_3 (int *index, const int i, const int j)
 {
     if (i == 0)
     {
-        index[0] = get_l (i, j - 1, m, n);
-        index[1] = get_l (i + 1, j - 1, m, n);
-        index[2] = get_l (i + 1, j, m, n);
+        index[0] = get_mesh_number (i, j - 1);
+        index[1] = get_mesh_number (i + 1, j - 1);
+        index[2] = get_mesh_number (i + 1, j);
     }
     else
     {
-        index[0] = get_l (i - 1, j, m, n);
-        index[1] = get_l (i - 1, j + 1, m, n);
-        index[2] = get_l (i, j + 1, m, n);
+        index[0] = get_mesh_number (i - 1, j);
+        index[1] = get_mesh_number (i - 1, j + 1);
+        index[2] = get_mesh_number (i, j + 1);
     }
 }
 
-void fill_in_matrix::pattern_4 (int *index, const int i, const int j, const int m, const int n)
+void fill_in_matrix::pattern_4 (int *index, const int i, const int j)
 {
     if (i == 0)
     {
-        index[0] = get_l (i, j - 1, m, n);
-        index[1] = get_l (i, j + 1, m, n);
-        index[2] = get_l (i + 1, j - 1, m, n);
-        index[3] = get_l (i + 1, j, m, n);
+        index[0] = get_mesh_number (i, j - 1);
+        index[1] = get_mesh_number (i, j + 1);
+        index[2] = get_mesh_number (i + 1, j - 1);
+        index[3] = get_mesh_number (i + 1, j);
     }
     if (j == 0)
     {
-        index[0] = get_l (i - 1, j, m, n);
-        index[1] = get_l (i - 1, j + 1, m, n);
-        index[2] = get_l (i, j + 1, m, n);
-        index[3] = get_l (i + 1, j, m, n);
+        index[0] = get_mesh_number (i - 1, j);
+        index[1] = get_mesh_number (i - 1, j + 1);
+        index[2] = get_mesh_number (i, j + 1);
+        index[3] = get_mesh_number (i + 1, j);
     }
-    if (i == m || i == cut_row_)
+    if (i == m_ || i == cut_row_)
     {
-        index[0] = get_l (i, j - 1, m, n);
-        index[1] = get_l (i - 1, j, m, n);
-        index[2] = get_l (i - 1, j + 1, m, n);
-        index[3] = get_l (i, j + 1, m, n);
+        index[0] = get_mesh_number (i, j - 1);
+        index[1] = get_mesh_number (i - 1, j);
+        index[2] = get_mesh_number (i - 1, j + 1);
+        index[3] = get_mesh_number (i, j + 1);
     }
-    if (j == n || j == cut_col_)
+    if (j == n_ || j == cut_col_)
     {
-        index[0] = get_l (i - 1, j, m, n);
-        index[1] = get_l (i + 1, j, m, n);
-        index[2] = get_l (i + 1, j - 1, m, n);
-        index[3] = get_l (i, j - 1, m, n);
-    }
+        index[0] = get_mesh_number (i - 1, j);
+        index[1] = get_mesh_number (i + 1, j);
+        index[2] = get_mesh_number (i + 1, j - 1);
+        index[3] = get_mesh_number (i, j - 1);
+      }
 }
 
-fill_in_matrix::fill_in_matrix (const int cut_row, const int cut_col, const double jacobian)
+double fill_in_matrix::rhs_pattern_count_6 (const int i, const int j)
+{
+  constexpr int triangular_num = 6;
+  constexpr int start_triangular = 0;
+  double result_sum = 0.;
+  for (int k = start_triangular; i < triangular_num; i++)
+    result_sum += compute_curr_triangular (k, i, j);
+
+  return result_sum;
+}
+
+double fill_in_matrix::rhs_pattern_count_6_special(const int i, const int j)
+{
+  constexpr int triangular_num = 5;
+  double result_sum = 0.;
+  constexpr int start_triangular = 3;
+  for (int k = start_triangular; i < start_triangular + triangular_num; i++)
+    result_sum += compute_curr_triangular (k % 6, i, j);
+
+  return result_sum;
+}
+
+double fill_in_matrix::rhs_pattern_count_4_up(const int i, const int j)
+{
+  constexpr int triangular_num = 3;
+  double result_sum = 0.;
+  constexpr int start_triangular = 2;
+  for (int k = start_triangular; i < start_triangular + triangular_num; i++)
+    result_sum += compute_curr_triangular (k, i, j);
+
+  return result_sum;
+}
+
+double fill_in_matrix::rhs_pattern_count_4_left(const int i, const int j)
+{
+  constexpr int triangular_num = 3;
+  double result_sum = 0.;
+  constexpr int start_triangular = 0;
+  for (int k = start_triangular; i < start_triangular + triangular_num; i++)
+    result_sum += compute_curr_triangular (k, i, j);
+
+  return result_sum;
+}
+
+double fill_in_matrix::rhs_pattern_count_4_down(const int i, const int j)
+{
+  constexpr int triangular_num = 3;
+  double result_sum = 0.;
+  constexpr int start_triangular = 0;
+  for (int k = start_triangular; i < start_triangular + triangular_num; i++)
+    result_sum += compute_curr_triangular (k, i, j);
+
+  return result_sum;
+}
+
+fill_in_matrix::fill_in_matrix (const int cut_row, const int cut_col, const int m, const int n, const double jacobian)
 {
   cut_row_ = cut_row;
   cut_col_ = cut_col;
   J_ = jacobian;
+  m_ = m;
+  n_ = n;
+  N_ = m_ * n_;
 }
 
 fill_in_matrix::~fill_in_matrix () {}
 
-int fill_in_matrix::get_nz_elements_current_node (const int i, const int j, const int row_cut, const int col_cut, const int m, const int n, int* index)
+int fill_in_matrix::get_nz_elements_current_node (const int i, const int j, int* index)
 {
-  if ((i > 0 && j > 0 && i < m && j < n && (i > row_cut && j < col_cut || i <= row_cut))) // FULL SUPP
-  {
-    pattern_1 (index, i, j, m, n);
-    return 6;
-  }
-  if ((i == 0 && j == 0) || (i == row_cut && j == n) || (i == m && j == col_cut)) // LEFT UP || RIGHT_DOWN_CUT || DOWN_RIGHT_CUT
-  {
-    pattern_2 (index, i, j, m, n);
-    return 2;
-  }
-  if ((i == m && j == 0) || (i == 0 && j == n)) // LEFT_DOWN || RIGHT_UP
-  {
-    pattern_3 (index, i, j, m, n);
-    return 3;
-  }
-  pattern_4 (index, i, j, m, n);
-  return 4;
+  nodes_type type_current_node_mesh = get_node_type (i, j);
+
+  switch (type_current_node_mesh)
+    {
+    case FULL_NODE:
+    case SPECIAL_POINT:
+      pattern_1 (index, i, j);
+      return 6;
+    case UP_WALL:
+    case LEFT_WALL:
+    case DOWN_WALL:
+    case RIGHT_WALL:
+      pattern_4 (index, i, j);
+      return 4;
+    case LEFT_UP_CORNER:
+    case RIGTH_DOWN_CORNER:
+      pattern_2(index, i, j);
+      return 2;
+    case LEFT_DOWN_CORNER:
+    case RIGHT_UP_CORNER:
+      pattern_3 (index, i, j);
+      return 3;
+    case UNDEFINED:
+      return 0;
+    }
+  return 0;
 }
 
-int fill_in_matrix::get_nozero_elements (const int m, const int n, const int my_rank, const int total_thread)
+int fill_in_matrix::get_nozero_elements (const int my_rank, const int total_thread)
 {
-    int i1 = my_rank * m;
+    int i1 = my_rank * m_;
     i1 /= total_thread;
-    int i2 = (my_rank + 1) * m;
-    i2 = i2 / p - 1;
+    int i2 = (my_rank + 1) * m_;
+    i2 = i2 / total_thread - 1;
 
     int nz = 0;
 
     for (int i = i1; i <= i2; i++)
     {
-        for (int j = 0; j <= n; j++)
+        for (int j = 0; j <= n_; j++)
         {
-            nz += get_nz_elements_current_node (i, j, cut_row_, cut_col_, m, n);
+            nz += get_nz_elements_current_node (i, j);
         }
     }
     reduce_sum (&nz, 1, total_thread);
     return nz;
 }
 
-int fill_in_matrix::fill_indexes (const int m, const int n, int *indexes)
+int fill_in_matrix::fill_indexes (int *indexes)
 {
     constexpr int max_row_nz = 6;
     int ind_loc[max_row_nz];
 
     int global_index = 0;
-    int NZ = m * n;
+    int NZ = N_;
     int nz = 0;
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m_; i++)
     {
-        for (int j = 0; j < n; j++, global_index++)
+        for (int j = 0; j < n_; j++, global_index++)
         {
-          nz = get_nz_elements_current_node (i, j, cut_row_, cut_col_, m, n, ind_loc);
+          nz = get_nz_elements_current_node (i, j, ind_loc);
           indexes[global_index] = NZ;
           for (int s = 0; s < nz; s++)
           {
@@ -148,12 +217,12 @@ int fill_in_matrix::fill_indexes (const int m, const int n, int *indexes)
     return NZ;
 }
 
-void fill_in_matrix::fillin_msr_matrix (double *msr_matrix, const int *indexes, const int m, const int n, const int nz, const int my_rank, const int total_thread)
+void fill_in_matrix::fillin_msr_matrix (double *msr_matrix, const int *indexes, const int my_rank, const int total_thread)
 {
-  const int N = n * m;
-  int i1 = my_rank * N;
+  const int n_ = n_ * m_;
+  int i1 = my_rank * n_;
   i1 /= total_thread;
-  int i2 = (my_rank + 1) * N;
+  int i2 = (my_rank + 1) * n_;
   i2 = i2 / total_thread - 1;
   const double I0 = (1. / 2) * J_;
   const double I1 = (1. / 12) * J_;
@@ -172,11 +241,52 @@ void fill_in_matrix::fillin_msr_matrix (double *msr_matrix, const int *indexes, 
     }
 }
 
-void fill_in_matrix::fillin_rhs (double *rhs, int n, const int my_rank, const int total_thread)
+nodes_type fill_in_matrix::get_node_type(const int i, const int j)
 {
-  int i1 = my_rank * N;
+  if ((i > 0 && j > 0 && i < m_ && j < n_ && ((i > cut_row_ && j < cut_col_) || i <= cut_row_)))
+    return nodes_type::FULL_NODE;
+
+  if (i == 0 && j > 0 && j < 0)
+    return nodes_type::UP_WALL;
+
+  if (j == (n_ - 1) && i > 0 && i < cut_row_)
+    return nodes_type::RIGHT_WALL;
+
+  if (i == cut_row_ && j > cut_col_ && j < n_ - 1)
+    return nodes_type::DOWN_WALL;
+
+  if (j == cut_col_&& i < (m_ - 1) && i > cut_row_)
+    return nodes_type::RIGHT_WALL;
+
+  if (i == m_ - 1 && j < cut_col_ && j > 0)
+    return nodes_type::DOWN_WALL;
+
+  if (j == 0 && i > 0 && i < m_ - 1)
+    return nodes_type::LEFT_WALL;
+
+  if (i == 0 && j == 0)
+    return nodes_type::LEFT_UP_CORNER;
+
+  if (i == 0 && j == n_ - 1)
+    return nodes_type::RIGHT_UP_CORNER;
+
+  if ((i == cut_row_ && j == n_ - 1) || (i == m_ - 1 && j == cut_col_))
+    return nodes_type::RIGTH_DOWN_CORNER;
+
+  if (i == m_ - 1 && j == 0)
+    return nodes_type::LEFT_DOWN_CORNER;
+
+  if (i == cut_row_ && j == cut_col_)
+    return nodes_type:: SPECIAL_POINT;
+
+  return nodes_type::UNDEFINED;
+}
+
+void fill_in_matrix::fillin_rhs (double *rhs, const int my_rank, const int total_thread)
+{
+  int i1 = my_rank * N_;
   i1 /= total_thread;
-  int i2 = (my_rank + 1) * N;
+  int i2 = (my_rank + 1) * N_;
   i2 = i2 / total_thread - 1;
 
   for (int i = i1; i <= i2; i++)
@@ -184,33 +294,79 @@ void fill_in_matrix::fillin_rhs (double *rhs, int n, const int my_rank, const in
       rhs[i] = rhs_element (i);
     }
 
-  synchronize(total_thread);
+  synchronize (total_thread);
 }
 
-double fill_in_matrix::rhs_element(const int global_num)
+double fill_in_matrix::rhs_element (const int global_num)
 {
   int i = 0;
   int j = 0;
 
   get_coord_mesh (global_num, i, j);
 
-  if ((i > 0 && j > 0 && i < m && j < n && (i > row_cut && j < col_cut || i <= row_cut))) // FULL SUPP
-  {
-    pattern_1 (index, i, j, m, n);
-    return 6;
-  }
-  if ((i == 0 && j == 0) || (i == row_cut && j == n) || (i == m && j == col_cut)) // LEFT UP || RIGHT_DOWN_CUT || DOWN_RIGHT_CUT
-  {
-    pattern_2 (index, i, j, m, n);
-    return 2;
-  }
-  if ((i == m && j == 0) || (i == 0 && j == n)) // LEFT_DOWN || RIGHT_UP
-  {
-    pattern_3 (index, i, j, m, n);
-    return 3;
-  }
-  pattern_4 (index, i, j, m, n);
-  return 4;
+  nodes_type type_current_node = get_node_type (i, j);
+
+  switch (type_current_node)
+    {
+    case FULL_NODE:
+      return rhs_pattern_count_6 (i, j);
+    case SPECIAL_POINT:
+      return rhs_pattern_count_6_special (i, j);
+    case UP_WALL:
+      return rhs_pattern_count_4_up (i, j);
+    case LEFT_WALL:
+      return rhs_pattern_count_4_left (i, j);
+    case DOWN_WALL:
+      return rhs_pattern_count_4_down (i, j);
+    case RIGHT_WALL:
+      return rhs_pattern_count_4_right (i, j);
+    case LEFT_UP_CORNER:
+      return rhs_pattern_count_2_left (i, j);
+    case RIGTH_DOWN_CORNER:
+      return rhs_pattern_count_2_right (i, j);
+    case LEFT_DOWN_CORNER:
+      return rhs_pattern_count_3_left (i, j);
+    case RIGHT_UP_CORNER:
+      return rhs_pattern_count_3_right (i, j);
+    case UNDEFINED:
+      return 0;
+    }
 }
 
+int fill_in_matrix::get_mesh_number (const int pos_x, const int pos_y)
+{
+    int global_mesh_position = 0;
+    if (pos_y > cut_row_)
+      {
+        global_mesh_position = cut_col_ * n_;
+        global_mesh_position += (pos_y - cut_row_) * cut_col_;
+      }
+    else
+      {
+        global_mesh_position = pos_y * n_;
+      }
 
+    global_mesh_position += pos_x;
+
+    return global_mesh_position;
+}
+
+int fill_in_matrix::get_coord_mesh (const int global_pos, int &pos_x, int &pos_y)
+{
+    if (global_pos < 0)
+        return -1;
+    int row = global_pos / n_;
+
+    if (row > cut_row_)
+    {
+        row = cut_col_ + (global_pos - cut_row_ * n_) / cut_col_;
+        pos_y = row;
+        pos_x = global_pos - (cut_row_ * n_ + (pos_y - cut_row_) * cut_col_);
+    }
+    else
+    {
+        pos_y = row;
+        pos_x = global_pos - pos_y * n_;
+    }
+    return 0;
+}
--- fill_in_matrix.h
+++ fill_in_matrix.h
@@ -1,32 +1,63 @@
 #ifndef FILL_IN_MATRIX_H
 #define FILL_IN_MATRIX_H
 
-
+enum nodes_type
+{
+  UNDEFINED,
+  FULL_NODE,
+  LEFT_UP_CORNER,
+  RIGTH_DOWN_CORNER,
+  LEFT_DOWN_CORNER,
+  RIGHT_UP_CORNER,
+  UP_WALL,
+  RIGHT_WALL,
+  DOWN_WALL,
+  LEFT_WALL,
+  SPECIAL_POINT
+};
 
 class fill_in_matrix
 {
 
 private:
+    int m_;
+    int n_;
+    int N_;
     int cut_row_;
     int cut_col_;
     double J_;
+    double (*function_)(double, double);
 
-    void pattern_1 (int* index, const int i, const int j, const int m, const int n);
-    void pattern_2 (int* index, const int i, const int j, const int m, const int n);
-    void pattern_3 (int* index, const int i, const int j, const int m, const int n);
-    void pattern_4 (int* index, const int i, const int j, const int m, const int n);
+    void pattern_1 (int* index, const int i, const int j);
+    void pattern_2 (int* index, const int i, const int j);
+    void pattern_3 (int* index, const int i, const int j);
+    void pattern_4 (int* index, const int i, const int j);
+
+    double rhs_pattern_count_6 (const int i, const int j);
+    double rhs_pattern_count_6_special (const int i, const int j);
+    double rhs_pattern_count_4_up (const int i, const int j);
+    double rhs_pattern_count_4_left (const int i, const int j);
+    double rhs_pattern_count_4_down (const int i, const int j);
+    double rhs_pattern_count_4_right (const int i, const int j);
+    double rhs_pattern_count_2_left (const int i, const int j);
+    double rhs_pattern_count_2_right (const int i, const int j);
+    double rhs_pattern_count_3_left (const int i, const int j);
+    double rhs_pattern_count_3_right (const int i, const int j);
 
 public:
-    fill_in_matrix (const int cut_row, const int cut_col, const double jacobian);
+    fill_in_matrix (const int cut_row, const int cut_col, const int m, const int n, const double jacobian);
     ~fill_in_matrix ();
 
-    int get_nz_elements_current_node (const int i, const int j, const int row_cut, const int col_cut, const int m, const int n, int *index = 0);
-    int get_nozero_elements (const int m, const int n, const int my_rank, const int total_thread);
-    int fill_indexes (const int m, const int n, int *indexes);
-    void fillin_msr_matrix (double *msr_matrix, const int *indexes, const int m, const int n, const int nz, const int my_rank, const int total_thread);
+    int get_nz_elements_current_node (const int i, const int j, int *index = 0);
+    int get_nozero_elements (const int my_rank, const int total_thread);
+    int fill_indexes (int *indexes);
+    void fillin_msr_matrix (double *msr_matrix, const int *indexes, const int my_rank, const int total_thread);
+    nodes_type get_node_type (const int i, const int j);
     void set_msr_matrix ();
-    void fillin_rhs (double *rhs, int n, const int my_rank, const int total_thread);
+    void fillin_rhs (double *rhs, const int my_rank, const int total_thread);
     double rhs_element (const int global_num);
+    int get_mesh_number (const int pos_x, const int pos_y);
+    int get_coord_mesh (const int global_pos, int &pos_x, int &pos_y);
 };
 
 #endif // FILL_IN_MATRIX_H
